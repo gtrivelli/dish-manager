8f56c49cc7f9eab1cc0c540027216aafa4693913
Gonzalo Trivelli <gtrivell136@gmail.com>
2025-09-09 21:10:36 -0400

    Load path froms env to support running on startup

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import sys
+import os
 from PyQt6.QtWidgets import QApplication
 from dish_manager import DishManager
 from dotenv import load_dotenv
 
 if __name__ == "__main__":
-    load_dotenv()
+    # Get the directory where this script is located
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    
+    # Load .env file from the script directory
+    load_dotenv(os.path.join(script_dir, '.env'))
     
     app = QApplication(sys.argv)
     
